15e5643385e72393c7193cbc1005927c1da27d77
dev-vedansh <dev.vedansh.kandpal@gmail.com>
2025-03-03 11:30:59 +0000

    datatype summary

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,28 @@
+// primitive
+// 7 types : String Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100  // int
+const scoreValur = 100.3 
+
+const isIn = true
+const outTem = null
+let userEmail //undefined
+
+const id = Symbol('123')
+
+
+// Non primitive (reference)
+
+// Array, Objects, Function
+
+const hereos = ['shaktiman', 'naagraj']   //array
+
+const obj = {
+    name:'ved'
+}       //object
+
+const myFunction = function (){
+    console.log("hello");
+    
+}
