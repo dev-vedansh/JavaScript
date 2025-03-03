9eaaa00270e25da5b33bd753bddf185737eb3860
dev-vedansh <dev.vedansh.kandpal@gmail.com>
2025-03-03 09:25:51 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 const value="12";
-console.log(typeof value)
-console.log(value)
+// console.log(typeof value)
+// console.log(value)
 
 let newValue = Number(value)
-console.log(typeof newValue)
-console.log(newValue)
+// console.log(typeof newValue)
+// console.log(newValue)
 
 // 34 => 34
 // "33abc" => NaN
@@ -12,7 +12,28 @@ console.log(newValue)
 // false =>0
 
 const isLoggedIn = 1
-console.log(Boolean(isLoggedIn)) //true
+// console.log(Boolean(isLoggedIn)) //true
 
 const str = 2563
-console.log(typeof String(str))
+// console.log(typeof String(str))
+
+
+/* ***************Operations******************* */
+
+let num = 3;
+let negNum = -num;
+// console.log(negNum)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)   //power
+// console.log(2/2)
+// console.log(2%2)   
+
+// console.log("1"+2)    // 12
+// console.log(1 + "2")    // 12
+// console.log(1 + 2 + "2")    // 32
+// console.log(1 + "2" + 2)    // 122
+// console.log("1" + 2 + 2)    // 122
+
