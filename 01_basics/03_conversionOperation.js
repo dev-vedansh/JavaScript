fb90c6081243bf35379e7368d7fd27cf06f21955
dev-vedansh <dev.vedansh.kandpal@gmail.com>
2025-03-03 09:08:00 +0000

    conversions of data-type

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,3 @@
-//  string to Number
 const value="12";
 console.log(typeof value)
 console.log(value)
@@ -7,3 +6,13 @@ let newValue = Number(value)
 console.log(typeof newValue)
 console.log(newValue)
 
+// 34 => 34
+// "33abc" => NaN
+//  true => 1 
+// false =>0
+
+const isLoggedIn = 1
+console.log(Boolean(isLoggedIn)) //true
+
+const str = 2563
+console.log(typeof String(str))
